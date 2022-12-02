4ad424052dc29a3fc102f736ed95834250ae4291
vladimirmac <vladimirbodrin89@gmail.com>
2022-12-02 11:04:15 +0300

    1

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from class_query import Query
-from utils.utils import open_file
 import os
 app = Flask(__name__)
 
@@ -9,16 +8,14 @@ app = Flask(__name__)
 def post():
     req = request.json
     path = os.path.abspath(f'data/{req["file_name"]}')
-
-    method = getattr(Query, req['cmd1'], "Такого метода нет")
-    method = method(req['value1'], open_file(path))
-    if req.get('cmd2', None):
-        method2 = getattr(Query, req['cmd2'], "Такого метода нет")
-        method2 = method2(req['value2'], list(method))
-        return list(method2)
-    return list(method)
-
-
+    with open(path, 'r', encoding='utf-8') as file:
+        method = getattr(Query, req['cmd1'], "Такого метода нет")
+        method = method(req['value1'], file)
+        if req.get('cmd2', None):
+            method2 = getattr(Query, req['cmd2'], "Такого метода нет")
+            method2 = method2(req['value2'], list(method))
+            return jsonify(list(method2))
+        return jsonify(list(method))
 
 
 if __name__ == '__main__':
--- utils/utils.py
+++
@@ -1,7 +0,0 @@
-
-
-def open_file(file):
-    """Функция открытия файла"""
-    with open(file, 'r', encoding='utf-8') as f:
-        return f.readlines()
-
