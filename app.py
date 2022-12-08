e2ce200c8470890a0655b8d6e981c7ea1bc96afd
vladimirmac <vladimirbodrin89@gmail.com>
2022-12-08 09:33:41 +0300

    add typing and regex

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+
+import ast
+import os
 from flask import Flask, request, jsonify
 from class_query import Query
-import os
+
 app = Flask(__name__)
 
 
 @app.route('/perform_query', methods=['GET', 'POST'])
 def post():
-    req = request.json
+    req = request.data
+    req = req.decode()
+    req = ast.literal_eval(req)  # преобразование бинарной строки в словарь
     path = os.path.abspath(f'data/{req["file_name"]}')
+
     with open(path, 'r', encoding='utf-8') as file:
         method = getattr(Query, req['cmd1'], "Такого метода нет")
         result = method(req['value1'], file)
         if 'cmd2' in req:
             method2 = getattr(Query, req['cmd2'], "Такого метода нет")
-            result = method2(req['value2'], list(result))
+            result = method2(req['value2'], result)
         return jsonify(list(result))
 
 
--- class_query.py
+++ class_query.py
@@ -1,29 +1,40 @@
+import re
+from typing import Iterable, TextIO, Union
 
 
 class Query:
 
     @staticmethod
-    def filter(value, file):
+    def filter(value: str, data: TextIO) -> filter:
         """Фильтрация по значению"""
-        return filter(lambda x: value in x, file)
+        return filter(lambda x: value in x, data)
 
     @staticmethod
-    def map(value, data):
+    def map(value: str, data: Union[TextIO, Iterable]) -> map:
         """Вывод данных по номеру колонки"""
         return map(lambda x: x.split(' ')[int(value)], data)
 
     @staticmethod
-    def unique(value, data):
+    def unique(value: None, data: Union[TextIO, Iterable]) -> set:
         return set(data)
 
     @staticmethod
-    def sort(value, data):
+    def sort(value: str, data: Union[TextIO, Iterable]) -> list:
         """Сортировка"""
         reversed = value == 'asc'
         return sorted(data, reverse=reversed)
 
     @staticmethod
-    def limit(value, data):
-        """Лимит выводимых строк"""
-        return data[:int(value)]
+    def limit(value: str, data: Union[TextIO, Iterable]) -> list:
+        return list(data)[:int(value)]
+
+    @staticmethod
+    def regex(value: str, data: Union[TextIO, Iterable]) -> list:
+        """Поиск по регулярному выражению"""
+        reg_exp = re.compile(r"" + value)
+        result = []
+        for i in list(data):
+            if re.findall(reg_exp, i):
+                result.append(i)
+        return result
 
